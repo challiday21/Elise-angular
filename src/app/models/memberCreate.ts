094bf179fa8158513819d5fbce2e031a1cb1a847
Claire HALLIDAY <clairehalliday73@gmail.com>
2022-06-10 17:39:35 +0200

    Developing CREATE front-end submit

--- src/app/models/memberCreate.ts
+++ src/app/models/memberCreate.ts
@@ -3,10 +3,10 @@ export class MemberCreate {
     firstName: string;
     surname: string;
     codeDep: number;
-    typeMember: string;
-    taskMember: string;
+    typeMember: number;
+    taskMember: number;
 
-    constructor(paramFirstName: string, paramSurname: string, paramCodeDep: number, paramTypeMember: string, paramTaskMember: string) {
+    constructor(paramFirstName: string, paramSurname: string, paramCodeDep: number, paramTypeMember: number, paramTaskMember: number) {
         this.firstName = paramFirstName;
         this.surname = paramSurname;
         this.codeDep = paramCodeDep;
